f522c473aff2e7108d4f60319ca7165f7325f55c
rakafebriansy <kerenr445@gmail.com>
2024-07-23 18:37:59 +0700

    feat: adding button and headpone icon components

---
+++ src/components/Button.vue
@@ -0,0 +1,23 @@
+<template>
+    <button class="bg-[#7A9CA5] flex p-2 text-xs font-semibold text-white items-center gap-3">
+        <Headphone size="w-3"></Headphone>
+        <slot></slot>
+    </button>
+</template>
+
+
+<script>
+    import Headphone from './icons/Headphone.vue';
+
+    export default {
+        components: {
+            Headphone
+        },
+        props: {
+            withIcon: {
+                default: false,
+                type: Boolean
+            }
+        }
+    }
+</script>
--- src/views/Landing.vue
+++ src/views/Landing.vue
@@ -1,8 +1,11 @@
 <template>
     <h1>Landing views</h1>
+    <Button>CUSTOMER SERVICE</Button>
 </template>
 
 <script>
+import Button from '../components/Button.vue';
+
     export default {
         data() {
             return {
@@ -10,7 +13,7 @@
             }
         },
         components: {
-            
+            Button
         },
         methods: {
             
