c203ba799b63b12141dd043605f8e031b04b464f
rakafebriansy <kerenr445@gmail.com>
2024-08-10 22:34:04 +0700

    feat: navbar (mobile)

--- src/components/Button.vue
+++ src/components/Button.vue
@@ -1,6 +1,6 @@
 <template>
     <button class="bg-[#7A9CA5] flex p-2 text-xs font-semibold text-white items-center gap-3">
-        <component :is="icon" size="w-3"></component>
+        <component :is="icon" classname="w-3"></component>
         <slot></slot>
     </button>
 </template>
