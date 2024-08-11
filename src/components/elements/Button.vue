e7461c2ee173f3dc42d364fe40c9e147f37eaf03
rakafebriansy <kerenr445@gmail.com>
2024-08-11 20:54:03 +0700

    refactor: separate navbar from landing page

---
+++ src/components/elements/Button.vue
@@ -0,0 +1,18 @@
+<template>
+    <button class="bg-[#7A9CA5] flex p-2 text-xs font-semibold text-white items-center gap-3">
+        <component :is="icon" classname="w-3"></component>
+        <slot></slot>
+    </button>
+</template>
+
+
+<script>
+    export default {
+        props: {
+            icon: {
+                type: Object,
+                required: true
+            }
+        }
+    }
+</script>
